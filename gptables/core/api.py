b30617645cbdec58b0cb8b8d9ef7116d1033ce67
Alexander Newton <43876799+alexander-newton@users.noreply.github.com>
2020-01-28 17:36:33 +0000

    Update api.py

--- gptables/core/api.py
+++ gptables/core/api.py
@@ -1,5 +1,6 @@
 from gptables.core.themes import create_theme
 from gptables.core.themes import gptheme
+from gptables.core.themes import import_theme
 
 def produce_table(data, theme=gptheme, **kwargs):
 
