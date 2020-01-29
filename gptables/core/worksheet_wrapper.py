0745efe0e05f6788dc728fca17b8db852f89633e
fosted1 <david.foster@ons.gov.uk>
2020-01-29 12:00:15 +0000

    mend

--- gptables/core/worksheet_wrapper.py
+++ gptables/core/worksheet_wrapper.py
@@ -1,29 +1,24 @@
+import pandas as pd
 import numpy as np
 import xlsxwriter
 import yaml
 
 class Worksheet(xlsxwriter.Workbook):
     """
-    Wrapper for and XlsxWriter Workbook object. The Worksheets class has been
-    replaced by an alternative that cumulatively stores styles (Formats) and
-    data on a cell by cell basis.
+    Wrapper for and XlsxWriter Worksheet object. Enables a good practice table
+    (gptable) to be added to the worksheet. All elements of the table must be
+    supplied at once.
     """
     
     def __init__(self):
         super(Worksheet, self).__init__()
         
-        default_styles = {
-                "sheet_global":{
-                        "font_size":9,
-                        "font":"Arial"
-                        },
-                                
-                "title":{
-                        "bold":True
-                        },
-                }
+        default_styles = gptheme
     
-    def add_gptable(self, table, title, source, notes=None, styles=self.default_styles):
+    def write_gptable(self, table, title, source, notes=None, theme=self.default_styles):
+        """
+        
+        """
         
         # Merge user supplied styles with default styles before generating format objects
         
@@ -37,11 +32,5 @@ class Worksheet(xlsxwriter.Workbook):
         Could be moved to Workbook, if we decide to wrap this too.
         """
         pass
-    
-class gptable_styles:
-    """
-    See XlsxWriter [format properties](https://xlsxwriter.readthedocs.io/format.html)
-    for valid options.
-    """
-    def __init__(self, yaml_style_config):
-        
+
+
