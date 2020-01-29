14c55ab13bec536567e90405286d56279d98b309
Alexander Newton <43876799+alexander-newton@users.noreply.github.com>
2020-01-29 11:23:40 +0000

    Revert "Added cost of reoffending example"
    This reverts commit 5d2fa362409bfc3d71c7600a3a8efd4aa9f76d18.

--- gptables/core/api.py
+++ gptables/core/api.py
@@ -4,20 +4,6 @@ from gptables.core.themes import import_theme
 
 def produce_table(data, theme=gptheme, **kwargs):
 
-    """Produces worksheet
 
-    This is the main function that will take in data, theme information,
-    and keyword arguments. It calls upon the packages to return a formatted
-    ``.xlsx`` file.
-
-    Args:
-        data: A pandas dataframe object
-        theme: A gptables theme object
-        **kwargs: Other arguments such as ``headings``, ``index``, ``source``
-            that map features of the data to table elements.
-
-    Returns:
-        A worksheet object.
-    """
 
 
