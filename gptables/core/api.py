b31c0657df2ba3903534d5bbd783419ae89d3108
Alexander Newton <43876799+alexander-newton@users.noreply.github.com>
2020-01-29 11:23:40 +0000

    Revert "Update api.py"
    This reverts commit b30617645cbdec58b0cb8b8d9ef7116d1033ce67.

--- gptables/core/api.py
+++ gptables/core/api.py
@@ -1,6 +1,5 @@
 from gptables.core.themes import create_theme
 from gptables.core.themes import gptheme
-from gptables.core.themes import import_theme
 
 def produce_table(data, theme=gptheme, **kwargs):
 
