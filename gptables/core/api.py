0ae1d7f2b07bdc25eb35992691910c6233c179f1
fosted1 <david.foster@ons.gov.uk>
2020-01-30 16:18:35 +0000

    Fixed bug introduced by renaming Theme class

--- gptables/core/api.py
+++ gptables/core/api.py
@@ -1,6 +1,6 @@
-from gptables.core.themes import create_theme
-from gptables.core.themes import gptheme
-from gptables.core.themes import import_theme
+from gptables.core.theme import create_theme
+from gptables.core.theme import gptheme
+from gptables.core.theme import import_theme
 
 def produce_table(data, theme=gptheme, **kwargs):
 
