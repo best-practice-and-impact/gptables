cad84ac0423798cc32ff136dc13ac3b0fcb7c17e
fosted1 <david.foster@ons.gov.uk>
2020-01-31 10:58:21 +0000

    Added methods to set table attributes

--- gptables/core/gptable.py
+++ gptables/core/gptable.py
@@ -1,12 +1,26 @@
+import pandas as pd
+
 class GPTable:
-    def __init__(self, table, title, subtitle, population, units, source, notes):
+    def __init__(self,
+                 table,
+                 title,
+                 subtitles
+                 scope,
+                 units,
+                 source,
+                 notes=[],
+                 index_columns={2:0}):
         """
         Initialise GPTable object object
-        """           
-        self.index_columns = {}  # 0-indexed column mapped to index level
+        """  
         self.multilevel_index = False
-        self.column_headers = [] # All other columns
-        self.data = pd.DataFrame()
+        self.VALID_INDEX_LEVELS = [1, 2, 3]
+        
+        self.table_shape = (0, 0)
+         
+        self.index_columns = {}  # 0-indexed column mapped to index level
+        self._column_headings = [] # All other columns
+        self.table = pd.DataFrame()
         
         self.title = None
         self.subtitles = []
@@ -18,23 +32,91 @@ class GPTable:
         self.notes = []
         
         # Set attributes using methods
-        for key, value in kwargs.items():
-            getattr(self, 'set_' + key)(value)
+#        # Locals method uses an unordered dict, so best to do this manually
+#        for key, value in locals():
+#            getattr(self, 'set_' + key)(value)
         
-    def set_data(self, new_table):
+        self.set_table(table, index_columns)
+        self.set_title(title)
+        self.set_subtitles(subtitles)
+        self.set_scope(scope)
+        self.set_units(units)
+        self.set_source(source)
+        self.set_notes(notes)
+        
+    def set_table(self, new_table, new_index_columns=None):
         """
-        Set the data attribute. Overwrites any existing table.
+        Set the `table`, `table_shape`, `index_columns` attributes. Overwrites
+        existing values for these attributes.
         """
-        self.table = data
-    
+        if not isinstance(new_table, pd.DataFrame):
+            raise ValueError("`table` must be a pandas DataFrame")
+        self.table = new_table
+        self._set_table_shape()
+        
+        if new_index_columns is None:
+            new_index_columns = self.index_columns
+        self.set_index_columns(new_index_columns)
+        
     def set_index_columns(self, new_index_columns):
         """
-        Set the index columns attribute. Overwrites any existing values.
-        A str or dict must be supplied. The dict should map index level to a
-        list of 0-indexed column numbers. All other columns will be considered
+        Set the `index_columns` attribute. Overwrites any existing values.
+        An dict must be supplied. This dict should map index level to a
+        single 0-indexed column number. All other columns will be considered
         as data columns.
         """
-        self.index_columns = new_index_columns
+        if isinstance(new_index_columns, dict):
+            # Check if levels and values are valid
+            valid_levels = all(level in self.VALID_INDEX_LEVELS for level in new_index_columns.keys())
+            if not valid_levels:
+                msg = ("Each dict key must be a valid index level:"
+                       f" {VALID_INDEX_LEVELS}")
+                raise ValueError(msg)
+            
+            column_indexes = [col for col in new_index_columns.values()]
+            if not all(isinstance(int, col) for col in column_indexes):
+                raise ValueError("Column indexes must be single integers")
+                
+            valid_columns = all(self._valid_column_index(col) for col in column_indexes)
+            if not valid_columns:
+                msg = ("Out of range: `index_columns` must be levels mapped to"
+                       " valid 0-indexed column numbers")
+                raise ValueError(msg)
+            
+            if len(new_index_columns.keys() > 1):
+                self.multilevel_index = True
+            else:
+                self.multilevel_index = False
+            
+            self.index_columns = new_index_columns
+            self._set_column_headings()
+        else:
+            msg = ("`index_columns` must be a dict mapping a valid index level"
+                   " to a 0-indexed column number")
+            raise ValueError(msg)
+    
+    def _valid_column_index(self, column_index):
+        """
+        Check if `column_index` is valid, given the `table_shape`.
+        """
+        return column_index in range(self.table_shape[1])
+        
+    def _set_column_headings(self):
+        """
+        Sets the `column_headings` attribute to the list of column indexes that
+        are not set as `index_columns`.
+        """
+        index_cols = list(self.index_columns.values())
+        self._column_headings = [x for x in range(self.table_shape[1])] - index_cols
+
+    def _set_table_shape(self):
+        """
+        Subsequently resets the `index_columns` and `column_headings` attributes
+        to verify that they are valid for the new tables shape.
+        """
+        self.table_shape = self.table.shape
+        self.table_shape[0] + 1  # Account for column headings
+        self.set_index_columns(self, self.index_columns)
     
     def set_title(self, new_title):
         """
