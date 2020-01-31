c40a964a39a0e284ab93fd48eaa66116a589572d
David Foster <foster.dev999@gmail.com>
2020-01-31 18:34:28 +0000

    Tried to substitute Worksheet class

--- gptables/core/worksheet_wrapper.py
+++ gptables/core/worksheet_wrapper.py
@@ -1,33 +1,32 @@
 import pandas as pd
 import numpy as np
 import xlsxwriter
-import yaml
 
-class Worksheet(xlsxwriter.Workbook):
+from .gptable import GPTable
+
+class Worksheet(xlsxwriter.worksheet.Worksheet):
     """
-    Wrapper for and XlsxWriter Worksheet object. Enables a good practice table
-    (gptable) to be added to the worksheet. All elements of the table must be
-    supplied at once.
+    Wrapper for and XlsxWriter Worksheet object. Provides a method for writing
+    a good practice table (GPTable) to a Worksheet.
     """
     
     def __init__(self):
         super(Worksheet, self).__init__()
 
-    def write_gptable(self, gptable, theme): # Could supply a default theme here, if prefered to XlsxWrite default
+    def write_gptable(self, gptable):
         """
-        Write data from a gptable object to the worksheet using the specified
-        theme object for formatting.
+        Write data from a GPTable object to the worksheet using the specified
+        Theme object for formatting.
         """
-        
-        # Merge user supplied styles with default styles before generating format objects
+        if not isinstance(gptable, GPTable):
+            raise ValueError("`gptable` must be a gptable.GPTable object")
+        # TODO. Implement method
+        # Get Theme from Worbook
         
         # Work through elements that have been provided and write them to workbook
-        
-        
-        pass
     
-    def add_cover_page(self, cover_config):
-        """
-        Could be moved to Workbook, if we decide to wrap this too.
-        """
-        pass
+#    def write_cover_page(self, cover_config):
+#        """
+#        Write a cover page to the worksheet.
+#        """
+#        pass
