99f9bf100da32a0392db493f80654155a6c52bc6
David Foster <foster.dev999@gmail.com>
2020-01-31 18:33:56 +0000

    Added classes to package init

--- gptables/__init__.py
+++ gptables/__init__.py
@@ -1,5 +1,7 @@
 from gptables.core.theme import Theme
 from gptables.core.gptable import GPTable
+from gptables.core.workbook_wrapper import Workbook
+from gptables.core.worksheet_wrapper import Worksheet
 
 from gptables.core.api import (
         #datatypes
