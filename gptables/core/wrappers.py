0f88fd2eeff5227dff58ae9f9f262ccc62f8d58b
David Foster <foster.dev999@gmail.com>
2020-02-04 15:59:13 +0000

    Updated docstrings for wrappers

--- gptables/core/wrappers.py
+++ gptables/core/wrappers.py
@@ -6,7 +6,7 @@ from .gptable import GPTable
 
 class GPWorksheet(Worksheet):
     """
-    Wrapper for and XlsxWriter Worksheet object. Provides a method for writing
+    Wrapper for an XlsxWriter Worksheet object. Provides a method for writing
     a good practice table (GPTable) to a Worksheet.
     """        
 #    def write_cover_page(self, cover_config):
@@ -19,6 +19,16 @@ class GPWorksheet(Worksheet):
         """
         Write data from a GPTable object to the worksheet using the specified
         Theme object for formatting.
+        
+        Parameters
+        ----------
+        gptable : gptables.GPTable
+            object contianing elements of the gptable to be written to the
+            Worksheet
+        
+        Returns
+        -------
+        None
         """
         if not isinstance(gptable, GPTable):
             raise ValueError("`gptable` must be a gptables.GPTable object")
@@ -39,11 +49,28 @@ class GPWorksheet(Worksheet):
                 pos
                 )
         
-        pos = self.
+        pos = self._write_table_elements(
+                gptable,
+                pos
+                )
         
+        pos = self._write_element(
+                gptable.source,
+                theme.source_format,
+                pos)
+        
+        pos = self._write_element_list(
+                gptable.legend,
+                theme.legend_format,
+                pos
+                )
+        
+        pos = self._write_element_list(
+                gptable.notes,
+                theme.notes_format,
+                pos
+                )
 
-        
-    
     def _write_element(self, element, format_dict, pos):
         """
         Write the title element of a GPTable to the GPWorksheet.
@@ -92,35 +119,15 @@ class GPWorksheet(Worksheet):
         for element in element_list:
             self._smart_write(*pos, element, format_obj)
             
-            pos[0] += 1
+            pos[0] += 2
             
         return pos
     
-    def _write_units(self, units, format_dict, pos):
+    def _write_table_elements(self, gptable, pos):
         """
-        Writes units above each column heading.
-        
-        Parameters
-        ----------
-        units: str or dict
-            single unit (for all columns) or map of column to unit type
-            {str: str}
-        format_dict: dict
-            format to be applied to units
-        pos: tuple
-            the position of the worksheet cell to write the units to
-
-        Returns
-        -------
-        pos: tuple
-            new position to write next element from
-        """
-        pass
-    
-    def _write_table_element(self, gptable, pos):
-        """
-        Writes the table element of a GPTable. Uses the Workbook Theme, plus
-        any additional formatting associated with the GPTable.
+        Writes the table, scope and units elements of a GPTable. Uses the
+        Workbook Theme, plus any additional formatting associated with the
+        GPTable.
         
         Parameters
         ----------
@@ -134,11 +141,17 @@ class GPWorksheet(Worksheet):
         pos: tuple
             new position to write next element from
         """
-        # Get theme and additional format
+        # Get theme and additional formatting
         theme = self.theme
         table = gptable.table
         
-        pass
+        # Write scope and units
+        
+        # Write table
+        
+        pos[0] += 1
+        pos[1] = 0  # Reset to first sheet column
+        return pos
         
     def _smart_write(self, row, col, data, format_dict):
         """
@@ -190,23 +203,47 @@ class GPWorksheet(Worksheet):
     def _merge_dict(base_dict, update_dict):
         """
         Creates a new dictionary, by updating a base dictionary not in-place.
+        
+        Parameters
+        ----------
+        base_dict : dict
+            the base dictionary to be updated not in-place
+        update_dict : dict
+            the dictionary to update `base_dict` with
+        
+        Returns
+        -------
+        updated_dict : dict
+            copy of `base_dict` updated with `update_dict`
+        
         """
         updated_dict = base_dict.copy()
         return updated_dict.update(update_dict)
     
     @staticmethod
-    def _excel_string_width(str):
+    def _excel_string_width(string):
         """
         Calculate the rough length of a string in Excel character units.
         This is highly inaccurate and doesn't account for font size etc.
-    
+        
+        Parameters
+        ----------
+        string : str
+            string to calculate width in Excel for
+        
+        Returns 
+        -------
+        string_width : float
+            width of equivalent string in Excel
         """
-        string_width = len(str)
+        string_len = len(string)
     
-        if string_width == 0:
-            return 0
+        if string_len == 0:
+            excel_width = 0
         else:
-            return string_width * 1.1
+            excel_width =  string_len * 1.1
+        
+        return excel_width
 
 
 class GPWorkbook(Workbook):
@@ -223,6 +260,16 @@ class GPWorkbook(Workbook):
     def add_worksheet(self, name=None):
         """
         Overwrite add_worksheet() to create a GPWorksheet object.
+        
+        Parameters
+        ----------
+        name : str (optional)
+            name of the the worksheet to be created
+        
+        Returns
+        -------
+        worksheet : gptables.GPWorksheet
+            a worksheet object, which supports writing of GPTable objects
         """
         worksheet = super(GPWorkbook, self).add_worksheet(name, GPWorksheet)
         worksheet.theme = self.theme
@@ -232,6 +279,16 @@ class GPWorkbook(Workbook):
     def set_theme(self, theme):
         """
         Sets the theme for all GPTable objects written to the Workbook.
+        
+        Parameters
+        ----------
+        theme : gptables.Theme
+            a Theme object containing the formatting to be applied to GPTable
+            objects written to Worksheets within this Workbook
+        
+        Returns
+        -------
+        None
         """
         if not isinstance(theme, Theme):
             raise ValueError("`theme` must be a gptables.Theme object")
