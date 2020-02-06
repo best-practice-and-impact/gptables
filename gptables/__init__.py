e9c80183ba725b1410b3a06727b6fdd3b82941e3
David Foster <foster.dev999@gmail.com>
2020-02-06 19:36:38 +0000

    Added gptheme unpickling to package init

--- gptables/__init__.py
+++ gptables/__init__.py
@@ -2,6 +2,8 @@ from gptables.core.theme import Theme
 from gptables.core.gptable import GPTable
 from gptables.core.wrappers import GPWorkbook
 
+from gptables.themes.unpickle_themes import gptheme
+
 
 from gptables.core.api import (
         #datatypes
@@ -16,3 +18,4 @@ from gptables.core.api import (
         produce_workbook,
 	write_workbook
         )
+
