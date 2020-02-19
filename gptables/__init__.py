5c7269d5e7acb5c2e851c982b66af84d152b6f23
David Foster <foster.dev999@gmail.com>
2020-02-19 13:12:57 +0000

    Added helper to pickle themes

--- gptables/__init__.py
+++ gptables/__init__.py
@@ -2,7 +2,7 @@ from gptables.core.theme import Theme
 from gptables.core.gptable import GPTable
 from gptables.core.wrappers import GPWorkbook
 
-from gptables.themes.unpickle_themes import gptheme
+from gptables.helpers.unpickle_themes import gptheme
 
 
 from gptables.core.api import (
--- setup.py
+++ setup.py
@@ -1,51 +1,20 @@
 from setuptools import setup, find_packages
-from distutils.command.develop import develop
-from distutils.command.install import install
-import pickle
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
-
-
-class PostInstall(install):
-    @staticmethod
-    def pickle_gptheme():
-        import gptables
-        theme = gptables.Theme("./themes/gptheme.yaml")
-        pickle.dump(theme, open("./gptables/themes/gptheme.pickle", "wb"))
-
-    def run(self):
-        install.run(self)
-        
-        #Post install actions
-        self.pickle_gptheme()
-        
-class PostInstallDev(develop):
-    @staticmethod
-    def pickle_gptheme():
-        import gptables
-        theme = gptables.Theme("./themes/gptheme.yaml")
-        pickle.dump(theme, open("./gptables/themes/gptheme.pickle", "wb"))
-
-    def run(self):
-        install.run(self)
-        
-        #Post install actions
-        self.pickle_gptheme()
-
+    
 setup(
     name='gptables',
     version='0.1.0',
     author='David Foster',
     description='Simplifying good practice in statistical tables.',
     data_files=[
-            ("examples", ["addn_files/demos/iris.py", "addn_files/demos/iris.csv"]),
+            ("examples", [
+                    "addn_files/examples/iris.py",
+                    "addn_files/examples/iris.csv"
+                    ]),
             ("themes", ["addn_files/themes/gptheme.yaml"])
             ],
-    cmdclass={
-        'develop': PostInstallDev,
-        'install': PostInstall,
-    },
     packages=find_packages(),
     install_requires=required
 )
