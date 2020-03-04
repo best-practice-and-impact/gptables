e0f811c7aef88828f46b2149121aec5692c3c084
alexander-newton <alexander.newton@ons.gov.uk>
2020-03-04 14:26:31 +0000

    added __doc__

--- gptables/__init__.py
+++ gptables/__init__.py
@@ -19,3 +19,20 @@ from gptables.core.api import (
 	write_workbook
         )
 
+__doc__ = """
+
+gptables - a highly opinionated package for spreadsheet production
+==================================================================
+
+**gptables** is a highly opinionated python package. It produces .xlsx files from your pandas dataframes or using reticulate in R. You define the mapping from your data to elements of the table. It does the rest.
+
+gptables uses the official guidance https://gss.civilservice.gov.uk/policy-store/releasing-statistics-in-spreadsheets/ on good practice spreadsheets. It advocates a strong adherence to the guidance by restricting the range of operations possible. The default theme gptheme should accomodate most use cases. gptables is developed and maintained by the Best Practice and Impact division of the Office for National Statistics, UK.
+
+Main Features
+-------------
+You define your mapping with your data as a GPTable.
+
+You define the format of your mapping with a Theme, the default is gptheme.
+
+You write_workbook() to win.
+"""
