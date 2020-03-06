aa4a2312aabe73cfabb1925fcf64275ef69af9e2
David Foster <foster.dev999@gmail.com>
2020-03-06 13:14:42 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(file):
    with open(file) as f:
         return f.read()
     
-long_description = read_file("README.md")
+long_description = read_file("README.rst")
 version = read_file("VERSION")
 license_contents = read_file("LICENSE")
 required = read_requirements("requirements.txt")
