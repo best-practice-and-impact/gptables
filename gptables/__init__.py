1daafd617fba5b4f7f6ea10b34000e75e77959e1
David Foster <foster.dev999@gmail.com>
2020-07-08 10:16:25 +0100

    Fixed non-rst elements in init

--- gptables/__init__.py
+++ gptables/__init__.py
@@ -33,13 +33,17 @@ It produces ``.xlsx`` files from your ``pandas`` dataframes or using
 ``reticulate`` in R. You define the mapping from your data to elements of the
 table. It does the rest.
 
-``gptables`` uses the official (guidance on good practice spreadsheets)[https://gss.civilservice.gov.uk/policy-store/releasing-statistics-in-spreadsheets/].
+``gptables`` uses the official `guidance on good practice spreadsheets`_
 It advocates a strong adherence to the guidance by restricting the range of operations possible.
 The default theme ``gptheme`` should accomodate most use cases.
 However, the ``Theme`` object allows development of custom themes, where other formatting is required.
 
-``gptables`` is developed and maintained by the (`Best Practice and Impact`)[https://gss.civilservice.gov.uk/about-us/support-for-the-gss/]
+``gptables`` is developed and maintained by the `Best Practice and Impact`_
 division of the Office for National Statistics, UK.
+
+.. _`guidance on good practice spreadsheets`: https://gss.civilservice.gov.uk/policy-store/releasing-statistics-in-spreadsheets/
+
+.. _`Best Practice and Impact`: https://gss.civilservice.gov.uk/about-us/support-for-the-gss/
 
 
 
