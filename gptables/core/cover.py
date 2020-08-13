d3620c7e562dbf0c1f02c5e43bebea606bce79a7
David Foster <foster.dev999@gmail.com>
2020-08-13 16:51:13 +0100

    Add cover label attribute to Cover

--- gptables/core/cover.py
+++ gptables/core/cover.py
@@ -8,10 +8,12 @@ class Cover():
 
     Attributes
     ----------
+        cover_label (str): cover page tab label, defaults to Cover"
         title (str): cover page title
         intro (str): optional introductory text
         about (str): optional about/notes text
     """
+    cover_label: str = "Cover"
     title: str
     intro: List = None
     about: List = None
