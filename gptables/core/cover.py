04a96b40883c313f61916f6bf240b05b70936679
David Foster <foster.dev999@gmail.com>
2020-08-24 11:32:57 +0100

    Add additional_elements to Cover

--- gptables/core/cover.py
+++ gptables/core/cover.py
@@ -8,14 +8,18 @@ class Cover():
 
     Attributes
     ----------
-        title str: cover page title
-        intro List[str], optional: introductory text
-        about List[str], optional: about/notes text
-        contact List[str], optional: contact details text
-        cover_label (str): cover page tab label, defaults to Cover"
+    title str: cover page title
+    intro List[str], optional: introductory text
+    about List[str], optional: about/notes text
+    contact List[str], optional: contact details text
+    cover_label (str): cover page tab label, defaults to Cover
+    additional_elements List[str], optional: additional GPTable elements to
+        display in the contents table. Allowed elements are "subtitles",
+        "scope" and "source".
     """
     title: str
     intro: List = None
     about: List = None
     contact: List = None
     cover_label: str = "Cover"
+    additional_elements: List = None
