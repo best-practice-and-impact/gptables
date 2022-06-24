eaf1379a0907e05346796e4f76f9d1dfb84c040f
Jacob Cole <77634686+Jacobb164@users.noreply.github.com>
2022-06-24 11:55:36 +0100

    Update R example for new release (#171)
    * Update R example for new release
    
    * Fix table_notes and typo in R example
    
    Co-authored-by: Rowan Hemsi <rowan.hemsi@ons.gov.uk>

--- gptables/examples/R_install_dependency_file.R
+++ gptables/examples/R_install_dependency_file.R
@@ -1,10 +1,19 @@
-packagesReq <- c("dplyr", "reticulate", "magrittr")
-packageRefresh <- function(pkgList) {
+#' install packages from a list
+#' 
+#' @description install any packages from a list of packages which R does not have
+#' 
+#' @param pkglist list of packages to install
+#' 
+#' @return NA 
+#'  
+#' @export 
+
+packageRefresh <- function(pkglist) {
   if(length(names(sessionInfo()$otherPkgs))) {
     lapply(names(sessionInfo()$otherPkgs), function(pkgs) detach(paste0("package:", pkgs), 
                                                                  character.only = T))
   }
-  newPackages <- pkgList[!(pkgList %in% installed.packages()[,"Package"])]
+  newPackages <- pkglist[!(pkglist %in% installed.packages()[,"Package"])]
   if(length(newPackages)) {
     install.packages(newPackages)
   }
--- gptables/examples/iris.R
+++ gptables/examples/iris.R
@@ -1,6 +1,22 @@
+
+#Iris - R Example
+#----------------------
+
+#This example demonstrates use of the ``gptables.write_workbook`` function in R.
+#This API function is designed for production of consistently structured and formatted tables.
+
+#Summary statistics from the classic iris dataset are used to build a ``gptables$GPTable``
+#object. Elements of metadata are provided to the corresponding parameters of the class.
+#Where you wish to provide no metadata in required parameters, use ``None``.
+
+#Table formatting can be defined as a ``gptable$Theme``, which is passed to the API functions
+# using the ``theme`` parameter. Or you can reply on our default - gptheme.
+
+
 # Reticulate example works on R version 4.0.1 or less
 source("gptables/examples/R_install_dependency_file.R")
 
+packagesReq <- c("dplyr", "reticulate", "magrittr")
 packageRefresh(packagesReq)
 
 library("magrittr")
@@ -17,28 +33,37 @@ iris_df <- reticulate::r_to_py(
 )
 
 table_name = "iris_statistics"
-title = list("Mean", reticulate::py_dict("italic", TRUE), " Iris", "$$note2$$ sepal dimensions")
+title = "Mean Iris$$note2$$ sepal dimensions"
 subtitles = c("1936 Fisher, R.A; The use of multiple measurements in taxonomic problems$$note1$$",
-              "Just another subtitile")
+              "Just another subtitle")
 units = reticulate::dict(list("1" = "cm", "2" = "cm"), convert = FALSE)
+table_notes = reticulate::dict(list("0" = "$$note1$$", "2" = "$$note3$$"), convert = FALSE)
 scope = "Iris"
 souce = "Source: Office for Iris Statistics"
 index_columns  = reticulate::py_dict(reticulate::py_eval('2'), "Species")
 
-annotations = list(note1 = "I've got 99 problems and taxonomy is one.",
-                   note2 = "Goo Goo Dolls, 1998.")
-notes = list("This note hath no reference.")
 
-table = gpt$GPTable(table_name = table_name,
-                    table = iris_df,
+table = gpt$GPTable(table = iris_df,
+                    table_name = table_name,
                     title = title,
                     subtitles = subtitles,
                     units = units,
+                    table_notes = table_notes,
                     scope = scope,
                     source = souce,
-                    index_columns = index_columns,
-                    annotations = annotations,
-                    notes = notes)
+                    index_columns = index_columns)
 
+notes = reticulate::dict(list("Note reference" = c("note1", "note2", "note3"),
+                              "Note text" = c("I've got 99 problems and taxonomy is one.", "Goo Goo Dolls, 1998.", "Just another note"),
+                              "Useful link" = c("[google](https://www.google.com)", "[duckduckgo](https://duckduckgo.com/)", "[ONS](https://www.ons.gov.uk)")),
+                         convert = FALSE)
 
-wb <- gpt$write_workbook(filename = "R_iris_gptable.xlsx", sheets = list("iris" = table))
+notes_table = pd$DataFrame$from_dict(notes)
+
+output_path <- "gptables/examples/R_iris_gptable.xlsx"
+
+gpt$write_workbook(filename = output_path,
+                         sheets = reticulate::dict(list("iris" = table)),
+                         notes_table = notes_table,
+                         contentsheet_options = reticulate::dict(list("additional_elements" = c("subtitles", "scope")))
+                         )
