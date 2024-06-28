1fea012c35cf813c6f34f88e86e73e68ba13c5c5
Milne <Daniel.Milne@ons.gov.uk>
2024-06-28 15:05:03 +0100

    Replaces penguins theme with default theme

--- gptables/examples/penguins_minimal_alternate.py
+++ gptables/examples/penguins_minimal_alternate.py
@@ -1,15 +1,9 @@
 """
-Penguins - Theme Example
+Penguins - Minimal Example
 ----------------------
 
-This example demonstrates how to use a custom theme in the production of a workbook.
-
-Summary statistics from the penguins dataset are used to build a ``gptables.GPTable``
-object. Elements of metadata are provided to the corresponding parameters of the class.
-Where you wish to provide no metadata in required parameters, use ``None``.
-
-The theme parameter must take either a directory or a yaml file in the ``gptables.write_workbook`` function. 
-The yaml file used in this example can be found in the themes folder as ''gptheme.yaml''.
+This example demonstrates  another way to use the ``gptables.write_workbook`` function.
+This code is equivalent to that in the above example.
 """
 
 import gptables as gpt
@@ -34,6 +28,7 @@ penguins_subtitles = [
 penguins_scope = "Penguins"
 penguins_source = "Palmer Station, Antarctica"
 
+#Use kwargs to pass these to the appropriate parameters
 kwargs = {
     "table_name": penguins_table_name,
     "title": penguins_title,
@@ -41,19 +36,18 @@ kwargs = {
     "scope": penguins_scope,
     "source": penguins_source,
     }
+
 penguins_table = gpt.GPTable(table=penguins_data, **kwargs)
 
+#Every table must be associated to a sheet name for writing
 penguins_sheets = {"Penguins": penguins_table}
 
 ## Use write_workbook to win!
-# Simply pass the filepath of the yaml file containing your theme to the GPTables Theme class and then to write_workbook
 if __name__ == "__main__":
     output_path = parent_dir / "python_penguins_gptable.xlsx"
-    theme_path = str(Path(__file__).parent.parent / "themes/gptheme.yaml")
     gpt.write_workbook(
         filename=output_path, 
         sheets=penguins_sheets,
-        theme = gpt.Theme(theme_path),
         contentsheet_options={"additional_elements": ["subtitles", "scope"]}
         )
     print("Output written at: ", output_path)
